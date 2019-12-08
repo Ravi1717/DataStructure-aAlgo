0bb8a2b0a125ee17010010ef60dc78ab69542ac7
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2019-12-08 19:03:27 +0530

    deletion of node

--- doublylinkedlist.js
+++ doublylinkedlist.js
@@ -5,6 +5,7 @@ function Node(data){
 }
 function doublylist(){
     this.size=0;
+
     this.head=null;
     this.tail=null;
 }
@@ -34,13 +35,50 @@ doublylist.prototype.searchnode=function(position){
             count++;
         }
         return currentnode;
-    }
-
+    };
+};
+doublylist.prototype.remove=function(position){
+  currentnode=this.head;
+  size=this.size;
+  count=1;
+  message={failure:"Failure:non-existent node in the list"};
+  beforenodetodelete=null;
+  nodetodelete=null;
+  deletednode=null;
+  if(size===0 || position<1 || position>size){
+    throw new Error(message.failure);
+}if(position===1){
+    this.head=currentnode.next;
+if(!this.head){
+    this.head.previous=null;
+}else{
+    this.tail=null;
 }
+}else if(position===this.size){
+    this.tail=this.tail.previous;
+    this.tail.next=null;
+}else{
+    while(count<position){
+       currentnode=currentnode.next;
+       count++; 
+    }
+    beforenodetodelete=currentnode.previous;
+    nodetodelete=currentnode;
+    afternodetodelete=currentnode.next;
+    beforenodetodelete.next=afternodetodelete;
+    afternodetodelete.previous=beforenodetodelete;
+    deletednode=nodetodelete;
+    nodetodelete=null;
+}
+this.size--;
+};
+
 var dl=new doublylist;
 dl.add(10);
 dl.add(20);
 dl.add(30);
 dl.add(40);
-console.log(dl.searchnode(2));
+dl.remove(3);
+console.log(dl)
+//console.log(dl.searchnode(2));
 //console.log(dl);
