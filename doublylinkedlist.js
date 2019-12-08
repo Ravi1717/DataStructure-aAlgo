886c3f6fccb8dd8a0861e5f19296da47a3645749
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2019-12-08 16:33:31 +0530

    Searching node

--- doublylinkedlist.js
+++ doublylinkedlist.js
@@ -21,9 +21,26 @@ doublylist.prototype.add=function(data){
     this.size++;
     }
 }
+doublylist.prototype.searchnode=function(position){
+    currentnode=this.head;
+    size=this.size;
+    count=1;
+    message={failure:"failure:nonexistent node in the list"};
+    if(size===0 || position<1 || position>size){
+        throw new Error(message.failure);
+    }else{
+        while(count<position){
+            currentnode=currentnode.next;
+            count++;
+        }
+        return currentnode;
+    }
+
+}
 var dl=new doublylist;
 dl.add(10);
 dl.add(20);
 dl.add(30);
 dl.add(40);
-console.log(dl);
+console.log(dl.searchnode(2));
+//console.log(dl);
